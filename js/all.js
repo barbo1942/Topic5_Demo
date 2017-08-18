68fbbe21166c9b55579514c2a283f75af15fa10e
barbo1942 <barbo19421942@gmail.com>
2017-08-18 11:15:25 +0800

    Update 2017-08-18T03:15:22.626Z

---
+++ js/all.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var Fn = function Fn() {
+  console.log('67');
+};
+
+Fn();
+'use strict';
+
+var Fn2 = function Fn2() {
+  console.log('g');
+};
+
+Fn2();
+//# sourceMappingURL=all.js.map
